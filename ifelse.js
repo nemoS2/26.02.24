5646bc1448f1c1810523eb1080a28425e1452da1
nemoS2 <142436312+nemoS2@users.noreply.github.com>
2024-02-26 12:35:40 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,23 @@
+// var x = 1; eh uma variavel e eh declarada e inicializada com o valor 1.
+var x = 1;
+
+
+// var y = 1; eh uma variavel e eh declarada e inicializada com o valor 1.
+var y = 1;
+
+
+// Aqui, há uma estrutura condicional if. Ela verifica se o valor da variável x é igual ao valor da variável y. A condição (x == y) verifica a igualdade, e se for verdadeira, o bloco de código dentro das chaves {} após o if será executado.
+if (x == y) {
+
+
+    // Se a condição x == y for verdadeira, ou seja, se os números forem iguais, esta instrução console.log será executada, imprimindo no console a mensagem "Números conferem!".
+    console.log("Números conferem!");
+
+
+// Se a condição do if for falsa, o bloco de código dentro das chaves {} após o else será executado.
+} else {
+
+
+    // Se os números não forem iguais, esta instrução console.log será executada, imprimindo no console a mensagem "Números divergem!".
+    console.log("Números divergem!");
+}
